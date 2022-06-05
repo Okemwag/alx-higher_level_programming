03f741135fb609abb47167f02c20db1a60ba490c
Okemwag <gabrielokemwa83@gmail.com>
2022-06-05 11:07:43 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    """
+    A function that finds the biggest integer of a list
+    """
+    if len(my_list) == 0:
+        return None
+    Largest = my_list[0]
+    for i in range(len(my_first)):
+        if my_list[i] > largest:
+            largest = my_list[i]
+    return (largest)
