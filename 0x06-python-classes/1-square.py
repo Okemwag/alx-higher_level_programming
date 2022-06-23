729cf2fc2d766d71756c3b13c6124b784275d878
Okemwag <gabrielokemwa83@gmail.com>
2022-06-23 07:13:22 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A class Square that defines a square by:
+(based on 0-square.py)
+
+"""
+
+
+class Square:
+    """Square class with a private attribut -
+    size.
+
+    """
+
+    def __init__(self, size):
+        """Initializes the size variable as a private
+        instance attribute
+
+        """
+        self.__size = size
