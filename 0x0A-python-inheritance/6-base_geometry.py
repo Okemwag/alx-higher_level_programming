8b5ebc65d42f8138f6cef5eb2e666876b67f9aac
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-07-06 14:40:37 +0300

    Add files via upload

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Improve Geometry.
+Author: Kanneh
+
+"""
+
+
+class BaseGeometry:
+    """An empty class"""
+
+    def area(self):
+        """Raises an exception because...
+        area is not implemented
+
+        """
+        raise Exception("area() is not implemented")
