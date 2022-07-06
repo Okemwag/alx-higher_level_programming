d75f54f697daa6cf7bf5f4681b91657f15f116fb
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-07-06 14:36:47 +0300

    Add files via upload

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Author: Kanneh"""
+
+
+def lookup(obj):
+    """A function that returns the list of available
+    attributes and methods of an object
+    Returns a list object
+
+    """
+    return dir(obj)
