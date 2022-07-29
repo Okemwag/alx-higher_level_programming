9bbb88f42a2f213b285d9ae54a0c14b945ec8499
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-07-29 14:17:35 +0300

    Add files via upload

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let loop;
+for (loop = 0; loop < myArray.length; loop++) {
+  console.log(myArray[loop]);
+}
