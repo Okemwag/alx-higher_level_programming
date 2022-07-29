62cb8248042878474e35173f0324f85623fd92aa
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-07-29 13:12:15 +0300

    Add files via upload

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- create a user with restricted access
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
