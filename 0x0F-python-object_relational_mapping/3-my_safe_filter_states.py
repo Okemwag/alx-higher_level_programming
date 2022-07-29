97b283fdd856f5edc5aa888763d27b7d43cdcd06
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-07-29 13:19:18 +0300

    Add files via upload

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module uses the MySQLdb module to execute SQL commands.
+(Best way to access database in python)
+"""
+import sys
+import MySQLdb as sql
+
+
+def select_states():
+    """This method Uses mySQL queries to select states"""
+    host = "localhost"
+    port = 3306
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    name = sys.argv[4]
+    connection = sql.connect(host=host, user=user,
+                             passwd=passwd, db=sys.argv[3], port=port)
+    cur = connection.cursor()
+    sql_command = '''SELECT * FROM states WHERE
+    name = %s ORDER BY id ASC;'''
+    name = (name,)
+    cur.execute(sql_command, name)
+    result = cur.fetchall()
+
+    if result:
+        for states in result:
+            print(states)
+    cur.close()
+    connection.close()
+
+
+if __name__ == '__main__':
+    select_states()
