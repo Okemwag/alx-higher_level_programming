ef685e56125f5102a45d8c5e170a76851fb77f31
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-09-06 15:07:28 +0300

    Delete 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const square = Number(process.argv[2]);
-if (square) {
-  for (let i = 0; i < square; i++) {
-    console.log('X'.repeat(square));
-  }
-} else {
-  console.log('Missing size');
-}
