c1d1af488368cb415633b81a20f74deb7121e58f
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-09-06 15:06:48 +0300

    Delete 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let loop;
-for (loop = 0; loop < myArray.length; loop++) {
-  console.log(myArray[loop]);
-}
