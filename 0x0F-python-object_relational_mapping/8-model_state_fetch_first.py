f62317751a68a0556b7b20b05fe7e224cb9dfdf9
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-09-16 09:29:28 +0300

    Initial Commit

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Prints first State object from database hbtn_0e_6_usa, "Nothing" if none
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+
+if __name__ == "__main__":
+    """ Engine connection
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    """ Session handling
+    """
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """ Query
+    """
+    query = session.query(State).order_by(State.id).first()
+    if query:
+        print("{}: {}".format(query.id, query.name))
+    else:
+        print("Nothing")
+
+    session.close()
