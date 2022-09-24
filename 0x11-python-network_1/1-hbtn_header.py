f21bc7e1fef3fb84d358c5bfaf9ce804c2c0ac30
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-09-24 17:18:50 +0300

    Delete 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""This script reads a particular header value"""
-import urllib.request as lib
-import sys
-
-if __name__ == '__main__':
-    req = lib.Request(sys.argv[1])
-    with lib.urlopen(req) as response:
-        print(dict(response.headers).get("X-Request-Id"))
