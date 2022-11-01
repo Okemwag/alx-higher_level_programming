945c03193462020aad59f3498e376dde3ca2a53d
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-11-01 08:17:48 +0300

    JavaScript - Web jQuery

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,2 @@
+// JavaScript script that updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag DIV#red_header
+$('#red_header').click(() => $('header').css('color', '#FF0000'));
