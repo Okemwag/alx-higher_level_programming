9e712e2cfa9df1ac39d4963ebbaeba86c9b06931
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-12-20 14:19:48 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+// Get the file path from the command line argument
+const filePath = process.argv[2];
+
+// Read the file in utf-8 encoding
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
-    console.log(err);
+    // Print the error object if there was an error while reading the file
+    console.error(err);
   } else {
+    // Print the file content if the reading was successful
     console.log(data);
   }
-})
-;
+});
