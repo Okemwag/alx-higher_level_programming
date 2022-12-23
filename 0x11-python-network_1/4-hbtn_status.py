d17c0219bd8e2add6ff946b97d87c2d8a99cba6b
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-12-23 10:57:26 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,6 +4,6 @@
 if __name__ == "__main__":
     import requests
     print("Body response:")
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
