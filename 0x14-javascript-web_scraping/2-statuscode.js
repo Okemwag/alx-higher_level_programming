a4d3bb34b66c5cb926539c04d564cc758d61214f
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-12-25 11:29:58 +0300

    Delete 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-const url = process.argv[2];
-
-request
-  .get(url)
-  .on('response', (response) => {
-    console.log('code: ' + response.statusCode);
-  });
