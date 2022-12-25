4eafd8de7dddb1a1b9852b6d13dafbf2899ed375
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-12-25 11:30:27 +0300

    Delete 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-const fs = require('fs');
-const url = process.argv[2];
-const fileStream = fs.createWriteStream(process.argv[3]);
-
-request
-  .get(url)
-  .pipe(fileStream);
