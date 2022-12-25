9ca7813efed40a8efedf8d1bb1fcfc2fb209607d
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2022-12-25 11:11:49 +0300

    Delete 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Fetches website info using requests (must be installed)
-"""
-if __name__ == "__main__":
-    import requests
-    print("Body response:")
-    r = requests.get('https://alx-intranet.hbtn.io/status')
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
